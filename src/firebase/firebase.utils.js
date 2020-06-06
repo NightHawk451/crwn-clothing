fc310d5a7498ecb28e3674f0d5d06e740e12ea6f
Micah Pearce <micah.r.pearce@gmail.com>
2020-06-05 20:07:29 -0500

    L27: implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included are withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,6 +40,37 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async ( collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef)
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj)
+  });
+
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap= (collections) => {
+  const transformtedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformtedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator
+  }, {});
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,19 @@
-import SHOP_DATA from './shop.data'
+
+
+import ShopActionTypes from './shop.types'
+import ShopPage from '../../pages/shop/shop.component';
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA
+  collections: null
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      }
     default:
       return state;
   }
