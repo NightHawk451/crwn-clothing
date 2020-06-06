bfa54bcdcc1d090b4a5a6d9b75cb548f1674ffaf
Micah Pearce <micah.r.pearce@gmail.com>
2020-06-05 21:52:09 -0500

    L28: updating code to leverage containers for collections-overview and collection-page components

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV == 'development') { // prevents logger from firing to console for production
   middlewares.push(logger);
