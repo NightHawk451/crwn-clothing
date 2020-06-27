7eaddd97a55f08151fb83ac9e66ff8f558405c2d
Micah Pearce <micah.r.pearce@gmail.com>
2020-06-27 14:02:02 -0500

    separating our cra app into client folder, add express server to help handle backend stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_JmzzAcnc2o0LgUAG8vAftyQL00EEApPk3s'
+  
+  const onToken = token => {
+    axios({ 
+      url: 'payment', // will take current directory and append payment to it (on own backend)
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(respons => {
+      alert("Payment Successful")
+    }).catch(error => {
+      console.log('Payment Error: ',JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please make sure you use the provided credit card'
+      )
+    })
+  }
+
+  return (<StripeCheckout
+    label='Pay Now'
+    name='CRWN Clothing Ltd.'
+    billingAddress
+    shippingAddress
+    image='https://svgshare.com/i/CUz.svg'
+    description={`Your total is $${price}`}
+    amount={priceForStripe}
+    panelLabel='Pay Now'
+    token={onToken}
+    stripeKey={publishableKey}
+  />) 
+}
+
+export default StripeCheckoutButton;
---
+++ client/src/firebase/firebase.utils.js
@@ -0,0 +1,90 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCSYESIFpc-FgcPkNU6DL_LcwgDoqxTH4Q",
+    authDomain: "crwn-db-f8b64.firebaseapp.com",
+    databaseURL: "https://crwn-db-f8b64.firebaseio.com",
+    projectId: "crwn-db-f8b64",
+    storageBucket: "crwn-db-f8b64.appspot.com",
+    messagingSenderId: "650808450516",
+    appId: "1:650808450516:web:2348fd9a0037fb991c0a41",
+    measurementId: "G-BKGMXYBLQX"
+  }
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+
+firebase.initializeApp(config);
+
+export const addCollectionAndDocuments = async ( collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef)
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj)
+  });
+
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap= (collections) => {
+  const transformtedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformtedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator
+  }, {});
+}
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => { //api listenere
+      unsubscribe();
+      resolve(userAuth);
+    }, reject)
+  })
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+export default firebase;
